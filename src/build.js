6e26e23ddac0624ec0ff236b59fcffd550ea2950
YouWei Zhao <zhao.uv@gmail.com>
2020-08-10 18:30:53 +0800

    languages

---
+++ src/build.js
@@ -0,0 +1,43 @@
+let lineRules = [
+    {
+        name: 'sharp',
+        RegExp: '#',
+        languages: [
+            { name: 'python', source: 'source.python', beginRegExp: '(?<=(?:^|\\G)(?:#\\s*%%\\s*\\[markdown\\]|#\\s*<markdowncell>)\\s*)$', endRegExp: '(^|\\G)(?=[^#\\s]|#\\s*%%|\\s+\\S)' },
+            { name: 'julia', source: 'source.julia' },
+            { name: 'shell', source: 'source.shell' },
+        ]
+    },
+    {
+        name: 'double-slash',
+        RegExp: '//',
+        languages: [
+            { name: 'js', source: 'source.js' },
+            { name: 'ts', source: 'source.ts' },
+            { name: 'c', source: 'source.c' },
+            { name: 'cpp', source: 'source.cpp' },
+            { name: 'csharp', source: 'source.csharp' },
+        ]
+    },
+]
+let blockRule = [
+    {
+        name: 'triple-quote',
+        beginRegExp: '"""',
+        endRegExp: ' ?"""',
+        languages: [
+            { name: 'python', source: 'python' },
+        ]
+    },
+    {
+        name: 'slash-star',
+        beginRegExp: '/\\*',
+        endRegExp: ' ?\\*/',
+        languages: [
+            { name: 'ts', source: 'source.ts' },
+            { name: 'c', source: 'source.c' },
+            { name: 'cpp', source: 'source.cpp' },
+            { name: 'csharp', source: 'source.csharp' },
+        ]
+    },
+]
