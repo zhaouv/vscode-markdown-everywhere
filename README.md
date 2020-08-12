e4eb9e413f424e0afe8d26f006e037a8bb4fc7df
YouWei Zhao <zhao.uv@gmail.com>
2020-08-12 15:02:15 +0800

    linerules-demo

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Embed and highlight markdown everywhere.
 demo.png...
 
 Extending from my pr in [vscode-python](https://github.com/microsoft/vscode-python) ([issue](https://github.com/microsoft/vscode-python/issues/4356)/[pr](https://github.com/microsoft/vscode-python/pull/13359)).
+
+# todo
+
+add onEnterRules
+
--- src/build.js
+++ src/build.js
@@ -1,8 +1,14 @@
+/* [markdown]
+ * block comment starting with the start mark is markdown
+ * this comment is the demo
+ * whileRegExp is optional
+ */
 let blockRules = [
     {
         name: 'triple-quote',
         beginRegExp: '"""',
         endRegExp: ' ?"""',
+        startMarkRegExp: '\\[markdown\\]',
         languages: [
             { name: 'python', source: 'python' },
         ]
@@ -10,7 +16,9 @@ let blockRules = [
     {
         name: 'slash-star',
         beginRegExp: '/\\*',
-        endRegExp: ' ?\\*/',
+        endRegExp: '\\*/',
+        whileRegExp: '\\*',
+        startMarkRegExp: '\\[markdown\\]',
         languages: [
             { name: 'ts', source: 'source.ts' },
             { name: 'c', source: 'source.c' },
@@ -19,13 +27,16 @@ let blockRules = [
         ]
     },
 ]
-
-let lineRules = [
+// [markdown]
+// all connecting line-comment starting with the start mark is markdown
+// this comment is the demo
+let lineRulesWithStartMark = [
     {
-        name: 'sharp',
+        name: 'number-sign',
         RegExp: '#',
+        startMarkRegExp: '\\[markdown\\]',
         languages: [
-            { name: 'python', source: 'source.python', beginRegExp: '(?<=(?:^|\\G)(?:#\\s*%%\\s*\\[markdown\\]|#\\s*<markdowncell>)\\s*)$', endRegExp: '(^|\\G)(?=[^#\\s]|#\\s*%%|\\s+\\S)' },
+            { name: 'python', source: 'source.python' },
             { name: 'julia', source: 'source.julia' },
             { name: 'shell', source: 'source.shell' },
         ]
@@ -33,6 +44,31 @@ let lineRules = [
     {
         name: 'double-slash',
         RegExp: '//',
+        startMarkRegExp: '\\[markdown\\]',
+        languages: [
+            { name: 'js', source: 'source.js' },
+            { name: 'ts', source: 'source.ts' },
+            { name: 'c', source: 'source.c' },
+            { name: 'cpp', source: 'source.cpp' },
+            { name: 'csharp', source: 'source.csharp' },
+        ]
+    },
+]
+//MD all connecting line-comment starting with the start mark is markdown
+//MD this comment is the demo
+let lineRulesStartsWith = [
+    {
+        name: 'number-sign',
+        RegExp: '#MD',
+        languages: [
+            { name: 'python', source: 'source.python' },
+            { name: 'julia', source: 'source.julia' },
+            { name: 'shell', source: 'source.shell' },
+        ]
+    },
+    {
+        name: 'double-slash',
+        RegExp: '//MD',
         languages: [
             { name: 'js', source: 'source.js' },
             { name: 'ts', source: 'source.ts' },
